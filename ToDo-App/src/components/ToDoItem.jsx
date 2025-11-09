a640c66035d8f973ffc04318a75a3edd137b88e0
Yuvraj <yj.yuvrajjangir@gmail.com>
2025-11-09 23:47:24 +0530

    more work

---
+++ ToDo-App/src/components/ToDoItem.jsx
@@ -0,0 +1,9 @@
+import './ToDoItem.css';
+function ToDoItem() {
+  return (
+    <div className="todo-item">
+      <p>Sample Task</p>
+    </div>
+  );
+}
+export default ToDoItem;
---
+++ ToDo-App/src/components/ToDoList.jsx
@@ -0,0 +1,9 @@
+import './ToDoList.css';
+function ToDoList() {
+  return (
+    <div className="todo-list">
+      <p>No tasks yet</p>
+    </div>
+  );
+}
+export default ToDoList;
